ed45b029158f6d884b9421538de0975182560294
Playtesch <73492821+Playtesch@users.noreply.github.com>
2020-12-10 14:01:33 +0100

    Commit 12
    Se añaden constantes globales para el programa y los comentarios de todo el código.

--- CACHEsym.c
+++ CACHEsym.c
@@ -4,23 +4,32 @@
 #include <string.h>
 #include <unistd.h>
 
+#define TAMANORAM 1024
+#define LINEASCACHE 4
+#define DATOSCACHE 8
+#define BITETIQUETA 5
+#define BITLINEA 2
+#define BITPALABRA 3
+
 
 typedef struct {
 	short int ETQ;
-	short int Datos[8];
+	short int Datos[DATOSCACHE];
 } T_LINEA_CACHE;
 
 void leerFicheroRAM(unsigned char RAM[]);
-void dividirRAM(unsigned char RAM[], unsigned char RAMOrdenada[][8]);
+void dividirRAM(unsigned char RAM[], unsigned char RAMOrdenada[][DATOSCACHE]);
 void datosDireccionDeMemoria(char direccionHex[], short int *direccion, short int* bloque, short int* etiqueta, short int* linea, short int* palabra);
 void traduccionBinario(char direccionHex[], char direccionBinario[]);
 void aciertoCache(T_LINEA_CACHE cache[], short int direccion, short int bloque, short int etiqueta, short int linea, short int palabra, float* tiempo, char texto[], int numaccesos);
-void falloCache(unsigned char RAM[][8], T_LINEA_CACHE cache[], short int direccion, short int bloque, short int etiqueta, short int linea, short int palabra, float* tiempo, int* numfallos);
+void falloCache(unsigned char RAM[][DATOSCACHE], T_LINEA_CACHE cache[], short int direccion, short int bloque, short int etiqueta, short int linea, short int palabra, float* tiempo, int* numfallos);
 void imprimirCache(T_LINEA_CACHE cache[]);
+
+//Jorge Tesch Torres MAIS2A
 int main(void) {
-    unsigned char RAM[1024];
-    unsigned char RAMOrdenada[128][8];
-	T_LINEA_CACHE cache[4];
+    unsigned char RAM[TAMANORAM];
+    unsigned char RAMOrdenada[TAMANORAM/DATOSCACHE][DATOSCACHE];
+	T_LINEA_CACHE cache[LINEASCACHE];
     FILE* ficheroAccesosDeMemoria;
     char valorAccesoDeMemoria[5];
     short int direccion, bloque, etiqueta, linea, palabra;
@@ -31,39 +40,48 @@ int main(void) {
     int acierto = 0;
 	int i, j;
 
-	for (i = 0; i < 4; i++) {
+    //Inicializamos el array de la cache.
+	for (i = 0; i < LINEASCACHE; i++) {
 		cache[i].ETQ = 0xFF;
-		for (j = 0; j < 8; j++) {
+		for (j = 0; j < DATOSCACHE; j++) {
 			cache[i].Datos[j] = 0;
 		}
 	}
 
+    //Leemos el fichero de la RAM y lo dividimos en los bloques coreespondientes.
 	leerFicheroRAM(RAM);
     dividirRAM(RAM, RAMOrdenada);
 
+    //Comprobamos si existe el fichero de los accesos de memoria y, si existe, lo abrimos y si no alertamos al usuario y salimos del programa.
     if ((ficheroAccesosDeMemoria = fopen("accesos_memoria.txt", "r")) == NULL) {
         printf("No existe el fichero de Accesos de Memoria\n");
         exit(-1);
     }
 
+    //Leemos las lineas del fichero de accesos de memoria de 1 en 1 hasta el final.
     while ((fread(valorAccesoDeMemoria, 5, 1, ficheroAccesosDeMemoria)) != NULL) {
         acierto = 0;
         numaccesos += 1;
 
+        //Obtenemos el bloque, etiqueta, linea y palabra de la direccion de memoria leida.
         datosDireccionDeMemoria(valorAccesoDeMemoria, &direccion, &bloque, &etiqueta, &linea, &palabra);
 
+        //Realizamos la comprobacion de la coincidecia de las etiquetas hasta que obtengamos el acierto.
         while (acierto == 0) {
             if (cache[linea].ETQ == etiqueta) {
+                //Si las etiquetas coinciden llamamos a la funcion de acierto.
                 aciertoCache(cache, direccion, bloque, etiqueta, linea, palabra, &tiempoglobal, texto, numaccesos);
                 acierto = 1;
             }
             else {
+                //Si las etiquetas no coinciden llamamos a la función de fallo.
                 falloCache(RAMOrdenada, cache, direccion, bloque, etiqueta, linea, palabra, &tiempoglobal, &numfallos);
             }
             sleep(2);
         }
     }
 
+    //Finalmente, imprimimos el tiempo global, el numero de accesos, el numero de fallos, el tiempo medio de acceso y el texto.
     printf("------------------------------------------------------------------------\n");
     printf("Tiempo Global: %.0f\nNumero de Accesos: %d\nNumero de Fallos: %d\nTiempo Medio de Acceso: %.2f\nTexto: ", tiempoglobal, numaccesos, numfallos, tiempoglobal / numaccesos);
     for (i = 0; i < numaccesos; i++) {
@@ -73,55 +91,67 @@ int main(void) {
 	return 0; 
 }
 
+//Función para leer el fichero de la RAM.
 void leerFicheroRAM(unsigned char RAM[]) {
 	FILE* fichero;
 
+    //Comprobamos si existe el fichero de la RAM y, si existe, lo abrimos y si no alertamos al usuario y salimos del programa.
 	if ((fichero = fopen("RAM.bin", "rb")) == NULL) {
 		printf("No existe el fichero de la RAM\n");
 		exit(-1);
 	}
 
-	fread(RAM, 1024, 1, fichero);
+    //Leemos todo el fichero y lo almacenamos en nuestro array RAM.
+	fread(RAM, TAMANORAM, 1, fichero);
 
 	fclose(fichero);
 	return;
 }
 
-void dividirRAM(unsigned char RAM[], unsigned char RAMOrdenada[][8]) {
+//Función para dividir el array de RAM en los bloques correspondientes.
+void dividirRAM(unsigned char RAM[], unsigned char RAMOrdenada[][DATOSCACHE]) {
     int i, j;
 
-    for (i = 0; i < 128; i++) {
-        for (j = 0; j < 8; j++) {
-            RAMOrdenada[i][j] = RAM[(i * 8) + j];
+    //Dividimos el array RAM segun el numero de bloques correspondientes a la cantidad de datos de la cache y los vamos almacenando en el nuevo array RAMOrdenada.
+    for (i = 0; i < (TAMANORAM / DATOSCACHE); i++) {
+        for (j = 0; j < DATOSCACHE; j++) {
+            RAMOrdenada[i][j] = RAM[(i * DATOSCACHE) + j];
         }
     }
 }
 
+//Función para extraer el bloque, etiqueta, linea y palabra de la direccion de memoria.
 void datosDireccionDeMemoria(char direccionHex[], short int *direccion, short int *bloque, short int *etiqueta, short int* linea, short int *palabra){
-	char direccionBinario[11];
-    char valorBloque[8], valorEtiqueta[6], valorLinea[3], valorPalabra[4];
+	char direccionBinario[BITETIQUETA+BITLINEA+BITPALABRA+1];
+    char valorBloque[BITETIQUETA+BITLINEA+1], valorEtiqueta[BITETIQUETA+1], valorLinea[BITLINEA+1], valorPalabra[BITPALABRA+1];
     int i;
 
+    //Primero traducimos la dirección de memoria de hexadecimal a binario.
     traduccionBinario(direccionHex, direccionBinario);
 
-    for (i = 0; i < 5; i++) {
+    //Copiamos los bits correspondientes de la direccion de memoria en binario en el array de etiqueta.
+    for (i = 0; i < BITETIQUETA; i++) {
         valorEtiqueta[i] = direccionBinario[i];
     }
-    valorEtiqueta[5] = '\0';
+    valorEtiqueta[BITETIQUETA] = '\0';
 
-    for (i = 0; i < 2; i++) {
-        valorLinea[i] = direccionBinario[i + 5];
+    //Copiamos los bits correspondientes de la direccion de memoria en binario en el array de linea.
+    for (i = 0; i < BITLINEA; i++) {
+        valorLinea[i] = direccionBinario[i + BITETIQUETA];
     }
-    valorLinea[2] = '\0';
+    valorLinea[BITLINEA] = '\0';
 
-    for (i = 0; i < 3; i++) {
-        valorPalabra[i] = direccionBinario[i + (7)];
+    //Copiamos los bits correspondientes de la direccion de memoria en binario en el array de palabra.
+    for (i = 0; i < BITPALABRA; i++) {
+        valorPalabra[i] = direccionBinario[i + (BITETIQUETA + BITLINEA)];
     }
-    valorPalabra[3] = '\0';
-
+    valorPalabra[BITPALABRA] = '\0';
+    
+    //Obtenemos el bloque concatenando la etiqueta y la linea.
     strcpy(valorBloque, valorEtiqueta);
     strcat(valorBloque, valorLinea);
 
+    //Transformamos los strings de direccion, bloque, etiqueta, linea y palabra a short int.
     *direccion = (short int)strtol(direccionHex, NULL, 16);
     *bloque = (short int)strtol(valorBloque, NULL, 2);
     *etiqueta = (short int)strtol(valorEtiqueta, NULL, 2);
@@ -129,10 +159,12 @@ void datosDireccionDeMemoria(char direccionHex[], short int *direccion, short in
     *palabra = (short int)strtol(valorPalabra, NULL, 2);
 }
 
+//Funcion para traducir la direccion de memoria de hexadecimal a binario.
 void traduccionBinario(char direccionHex[], char direccionBinario[]) {
     char valorBinario[5];
     int i;
 
+    //Transformamos de 1 en 1 los bytes de la dirección hexadecimal a binario.
     for (i = 1; i < 4; i++) {
         switch (direccionHex[i]) {
         case '0':
@@ -168,13 +200,13 @@ void traduccionBinario(char direccionHex[], char direccionBinario[]) {
         case 'F':
             strcpy(valorBinario, "1111"); break;
         }
-
+        //Ya que el bus de memoria es de 10 bits, inicialmente solo copiamos los 2 primeros bits del primer byte.
         if (i == 1) {
             direccionBinario[0] = valorBinario[2];
             direccionBinario[1] = valorBinario[3];
             direccionBinario[2] = '\0';
         }
-
+        //El resto de bytes los concatenamos completamente.
         else {
             strcat(direccionBinario, valorBinario);
         }
@@ -182,40 +214,43 @@ void traduccionBinario(char direccionHex[], char direccionBinario[]) {
 }
 
 
-
+//Funcion para manejar un acierto de la cache.
 void aciertoCache(T_LINEA_CACHE cache[], short int direccion, short int bloque, short int etiqueta, short int linea, short int palabra, float* tiempo, char texto[], int numaccesos) {
-
+    //Imprimimos las datos del acierto.
     printf("T: %.0f, Acierto de CACHE, ADDR %04X ETQ %X linea %02X palabra %02X DATO %02X\n", *tiempo, direccion, etiqueta, linea, palabra, cache[linea].Datos[7-palabra]);
-
+    //Imprimimos todos los elementos de la cache.
     imprimirCache(cache);
-
+    //Guardamos el dato en la variable texto.
     texto[numaccesos - 1] = cache[linea].Datos[7 - palabra];
     *tiempo += 1;
 }
 
-
-void falloCache(unsigned char RAM[][8], T_LINEA_CACHE cache[], short int direccion, short int bloque, short int etiqueta, short int linea, short int palabra, float* tiempo, int* numfallos) {
+//Funcion para manejar un fallo de la cache.
+void falloCache(unsigned char RAM[][DATOSCACHE], T_LINEA_CACHE cache[], short int direccion, short int bloque, short int etiqueta, short int linea, short int palabra, float* tiempo, int* numfallos) {
     int i;
 
     *numfallos += 1;
 
+    //Imprimimos las datos del fallo.
     printf("T: %.0f, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X\n", *tiempo, *numfallos, direccion, etiqueta, linea, palabra, bloque);
     printf("Cargando el bloque %02X en la linea %02X\n", bloque, linea);
 
     *tiempo += 10;
 
+    //Copiamos la etiqueta y los datos del bloque en la cache.
     cache[linea].ETQ = etiqueta;
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < DATOSCACHE; i++) {
         cache[linea].Datos[i] = RAM[bloque][7 - i];
     }
 }
 
+//Funcion para imprimir todos los datos de la cache.
 void imprimirCache(T_LINEA_CACHE cache[]) {
     int i, j;
 
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < LINEASCACHE; i++) {
         printf("ETQ: %X\tDatos ", cache[i].ETQ);
-        for (j = 0; j < 8; j++) {
+        for (j = 0; j < DATOSCACHE; j++) {
             printf("%02X ", cache[i].Datos[j]);
         }
         printf("\n");
